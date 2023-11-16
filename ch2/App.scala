64f2af78eab1eeea45f4f0dfe472830000e704bc
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-11-16 13:17:39 -0500

    added more to ch2

--- ch2/App.scala
+++ ch2/App.scala
@@ -1,3 +1,13 @@
+import scala.io.Source.fromFile
 
-@main def main(args: String*) =
-    for arg <- args do println(arg)
+// functions can be declared without any parameters
+def getNum = BigInt(123).toString
+
+// lazy value's initializations are deffered until they are
+// accessed for the first time
+lazy val words = fromFile("words.txt").mkString
+
+@main def main =
+
+    println(words)
+    println(getNum)
