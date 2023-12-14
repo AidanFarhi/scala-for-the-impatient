a8a17d3058b48a23d83276fa0e35f8c03055786f
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-12-14 17:17:12 -0500

    finished ex4

--- ch8/Ex4.scala
+++ ch8/Ex4.scala
@@ -1,5 +1,7 @@
 package ex4
 
+import scala.collection.mutable.ArrayBuffer
+
 /*
 4. Define an abstract class Item with methods price and description. 
 A SimpleItem is an item whose price and description are specified 
@@ -9,5 +11,33 @@ price is the sum of the prices in the bundle. Also provide a
 mechanism for adding items to the bundle and a suitable description method. 
 */
 
+abstract class Item:
+
+    def price: Double
+    def description: String
+
+
+class SimpleItem(val itemPrice: Double, val itemDescription: String) extends Item:
+
+    override def price: Double = itemPrice
+    override def description: String = itemDescription
+
+
+class Bundle extends Item:
+
+    private val items = ArrayBuffer[Item]()
+
+    def addItem(item: Item) = items.addOne(item)
+    override def price: Double = items.map(i => i.price).sum
+    override def description: String = "A collection of items"
+
+
 @main def ex4 =
-    println("hello")
+    
+    val bundle = Bundle()
+    bundle.addItem(SimpleItem(10.0, "a helmet"))
+    bundle.addItem(SimpleItem(23.0, "a sword"))
+    bundle.addItem(SimpleItem(93.2, "a shield"))
+
+    assert(bundle.price == 126.2)
+    assert(bundle.description == "A collection of items")
